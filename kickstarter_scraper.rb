7a89275d43a546afb07fed9facc28fd5a94ff8f2
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-30 00:04:11 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,7 +16,7 @@ def create_project_hash
     end
     projects
 end
-create_project_hash
+
 
 #projects : kickstarter.css(".li.project.grid_4").first
 #title : project.css("h2.bbcard_name strong a").text
